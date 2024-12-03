b9cff876ad3313f124e9d8e16a63be088cea5158
Davi Rios dos Santos <AstroSurfer.50@Gmail.com>
2024-12-03 14:53:05 -0300

    Revert "AJUSTE DEploy vite"
    This reverts commit 4db4bf397305ead3e0bfbbd7e66434c28d0a4d03.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(process.env.VITE_PORT) || 4173,
-    host: true, // Permite acesso externo
-  },
-});
+})
